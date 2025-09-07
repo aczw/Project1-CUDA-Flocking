2be2ab8b47082622c9c17de3405e89d0d8b4db7f
Charles Wang <zwcharl@gmail.com>
2025-09-07 16:29:18 -0400

    add script to automate running perf tests

---
+++ analysis/perform_analysis.ps1
@@ -0,0 +1,39 @@
+# If you're running this, make sure you do so from the repo root i.e. move this
+# script out of the "analysis" folder! The program uses hardcoded relative paths
+# to the "shaders" folder and will not find them otherwise.
+
+.\build\bin\release\cis5650_boids.exe 10000 64
+.\build\bin\release\cis5650_boids.exe 10000 128
+.\build\bin\release\cis5650_boids.exe 10000 256
+.\build\bin\release\cis5650_boids.exe 10000 512
+.\build\bin\release\cis5650_boids.exe 10000 1024
+.\build\bin\release\cis5650_boids.exe 25000 64
+.\build\bin\release\cis5650_boids.exe 25000 128
+.\build\bin\release\cis5650_boids.exe 25000 256
+.\build\bin\release\cis5650_boids.exe 25000 512
+.\build\bin\release\cis5650_boids.exe 25000 1024
+.\build\bin\release\cis5650_boids.exe 50000 64
+.\build\bin\release\cis5650_boids.exe 50000 128
+.\build\bin\release\cis5650_boids.exe 50000 256
+.\build\bin\release\cis5650_boids.exe 50000 512
+.\build\bin\release\cis5650_boids.exe 50000 1024
+.\build\bin\release\cis5650_boids.exe 100000 64
+.\build\bin\release\cis5650_boids.exe 100000 128
+.\build\bin\release\cis5650_boids.exe 100000 256
+.\build\bin\release\cis5650_boids.exe 100000 512
+.\build\bin\release\cis5650_boids.exe 100000 1024
+.\build\bin\release\cis5650_boids.exe 250000 64
+.\build\bin\release\cis5650_boids.exe 250000 128
+.\build\bin\release\cis5650_boids.exe 250000 256
+.\build\bin\release\cis5650_boids.exe 250000 512
+.\build\bin\release\cis5650_boids.exe 250000 1024
+.\build\bin\release\cis5650_boids.exe 750000 64
+.\build\bin\release\cis5650_boids.exe 750000 128
+.\build\bin\release\cis5650_boids.exe 750000 256
+.\build\bin\release\cis5650_boids.exe 750000 512
+.\build\bin\release\cis5650_boids.exe 750000 1024
+.\build\bin\release\cis5650_boids.exe 1500000 64
+.\build\bin\release\cis5650_boids.exe 1500000 128
+.\build\bin\release\cis5650_boids.exe 1500000 256
+.\build\bin\release\cis5650_boids.exe 1500000 512
+.\build\bin\release\cis5650_boids.exe 1500000 1024
