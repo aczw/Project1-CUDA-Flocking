e9dbcb68e6096db21b64c57c2b93bf0d45f611a0
Charles Wang <zwcharl@gmail.com>
2025-09-07 16:42:01 -0400

    actually learn powershell

--- analysis/perform_analysis.ps1
+++ analysis/perform_analysis.ps1
@@ -2,38 +2,12 @@
 # script out of the "analysis" folder! The program uses hardcoded relative paths
 # to the "shaders" folder and will not find them otherwise.
 
-.\build\bin\release\cis5650_boids.exe 10000 64
-.\build\bin\release\cis5650_boids.exe 10000 128
-.\build\bin\release\cis5650_boids.exe 10000 256
-.\build\bin\release\cis5650_boids.exe 10000 512
-.\build\bin\release\cis5650_boids.exe 10000 1024
-.\build\bin\release\cis5650_boids.exe 25000 64
-.\build\bin\release\cis5650_boids.exe 25000 128
-.\build\bin\release\cis5650_boids.exe 25000 256
-.\build\bin\release\cis5650_boids.exe 25000 512
-.\build\bin\release\cis5650_boids.exe 25000 1024
-.\build\bin\release\cis5650_boids.exe 50000 64
-.\build\bin\release\cis5650_boids.exe 50000 128
-.\build\bin\release\cis5650_boids.exe 50000 256
-.\build\bin\release\cis5650_boids.exe 50000 512
-.\build\bin\release\cis5650_boids.exe 50000 1024
-.\build\bin\release\cis5650_boids.exe 100000 64
-.\build\bin\release\cis5650_boids.exe 100000 128
-.\build\bin\release\cis5650_boids.exe 100000 256
-.\build\bin\release\cis5650_boids.exe 100000 512
-.\build\bin\release\cis5650_boids.exe 100000 1024
-.\build\bin\release\cis5650_boids.exe 250000 64
-.\build\bin\release\cis5650_boids.exe 250000 128
-.\build\bin\release\cis5650_boids.exe 250000 256
-.\build\bin\release\cis5650_boids.exe 250000 512
-.\build\bin\release\cis5650_boids.exe 250000 1024
-.\build\bin\release\cis5650_boids.exe 750000 64
-.\build\bin\release\cis5650_boids.exe 750000 128
-.\build\bin\release\cis5650_boids.exe 750000 256
-.\build\bin\release\cis5650_boids.exe 750000 512
-.\build\bin\release\cis5650_boids.exe 750000 1024
-.\build\bin\release\cis5650_boids.exe 1500000 64
-.\build\bin\release\cis5650_boids.exe 1500000 128
-.\build\bin\release\cis5650_boids.exe 1500000 256
-.\build\bin\release\cis5650_boids.exe 1500000 512
-.\build\bin\release\cis5650_boids.exe 1500000 1024
+$boids_counts = 10000, 25000, 50000, 100000, 250000, 750000, 150000
+$block_sizes = 64, 128, 256, 512, 1024
+
+foreach ($boid_count in $boids_counts) {
+    foreach ($block_size in $block_sizes) {
+        .\build\bin\release\cis5650_boids.exe "$boid_count" "$block_size"
+        Write-Host ""
+    }
+}
